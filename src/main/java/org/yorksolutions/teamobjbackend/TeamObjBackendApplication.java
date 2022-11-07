55ac4c3a4ff631fdcb4eed66c86d653bf3b21182
Max Feige <mfeige001@gmail.com>
2022-11-07 07:53:13 -0600

    Initialization

---
+++ src/main/java/org/yorksolutions/teamobjbackend/TeamObjBackendApplication.java
@@ -0,0 +1,15 @@
+package org.yorksolutions.teamobjbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamObjBackendApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TeamObjBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/yorksolutions/teamobjbackend/TeamObjBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package org.yorksolutions.teamobjbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamObjBackendApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
