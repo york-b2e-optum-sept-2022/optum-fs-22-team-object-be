57c5ff79410e69a1142163285a57d617cf6d5df0
Max Feige <mfeige001@gmail.com>
2022-11-07 08:57:23 -0600

    Added layout of product service with bunch of TODOs

--- src/main/java/org/yorksolutions/teamobjbackend/services/ProductService.java
+++ src/main/java/org/yorksolutions/teamobjbackend/services/ProductService.java
@@ -1,11 +1,59 @@
 package org.yorksolutions.teamobjbackend.services;
 
+import java.util.List;
+
 public class ProductService
 {
 
 
     //TODO:
-    public void DeleteCoupon(String id)
+    public void DeleteCoupon(String couponID)
+    {
+
+    }
+    //TODO
+    public void AddCoupon(Double salePercentage, List<String> productIDs, Long startDate, Long endDate)
+    {
+
+    }
+
+    //TODO:
+    public void AddMAPRange(String productID, Double MAP, Long startDate, Long endDate)
+    {
+
+    }
+    //TODO
+    public void DeleteMAPRange(String ProductID, Long date)
+    {
+
+    }
+    //TODO:
+    public void AddPriceRange(String productID, Double price, Long startDate, Long endDate)
+    {
+
+    }
+    //TODO
+    public void DeletePriceRange(String ProductID, Long date)
+    {
+
+    }
+    //TODO:
+    public void AddSaleRange(String productID, Double salePercent, Long startDate, Long endDate)
+    {
+
+    }
+    //TODO
+    public void DeleteSaleRange(String ProductID, Long date)
+    {
+
+    }
+    //TODO:
+    public void AddCategories(String categoryName, List<String> productIDs)
+    {
+
+    }
+    //TODO
+    public void DeleteCategories(String categoryName, List<String> productIDs)
     {
 
     }
