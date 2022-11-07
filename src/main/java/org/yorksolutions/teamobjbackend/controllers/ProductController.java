5faba44b09c540418488d92e8b0ef1933fac6e10
Max Feige <mfeige001@gmail.com>
2022-11-07 09:18:05 -0600

    Simple product DTO echo

--- src/main/java/org/yorksolutions/teamobjbackend/controllers/ProductController.java
+++ src/main/java/org/yorksolutions/teamobjbackend/controllers/ProductController.java
@@ -1,10 +1,19 @@
 package org.yorksolutions.teamobjbackend.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.yorksolutions.teamobjbackend.dtos.ProductDTO;
 
 @RestController
 @RequestMapping("/api/product/")
 public class ProductController
 {
+
+    @PostMapping
+    public ProductDTO CreateProduct(@RequestBody ProductDTO productData)
+    {
+        return productData;
+    }
 }
