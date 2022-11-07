c20a36f703128f2628e96e1d8f34291719219ae7
Max Feige <mfeige001@gmail.com>
2022-11-07 09:46:28 -0600

    Test created account

--- src/main/java/org/yorksolutions/teamobjbackend/controllers/AccountController.java
+++ src/main/java/org/yorksolutions/teamobjbackend/controllers/AccountController.java
@@ -1,33 +1,64 @@
 package org.yorksolutions.teamobjbackend.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.yorksolutions.teamobjbackend.dtos.AccountDTO;
 import org.yorksolutions.teamobjbackend.entities.Account;
 import org.yorksolutions.teamobjbackend.entities.AccountPermission;
 import org.yorksolutions.teamobjbackend.repositories.AccountRepository;
+import org.yorksolutions.teamobjbackend.repositories.ProductOrderRepository;
+import org.yorksolutions.teamobjbackend.utils.YorkUtils;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user/")
 public class AccountController
 {
 
-    private final AccountRepository
+    private final AccountRepository accountRepository;
+    private final ProductOrderRepository productOrderRepository;
+
+    @Autowired
+    public AccountController(AccountRepository accountRepository, ProductOrderRepository productOrderRepository)
+    {
+        this.accountRepository = accountRepository;
+        this.productOrderRepository = productOrderRepository;
+    }
 
     @PostMapping("register")
     public Account RegisterNewUser(@RequestBody AccountDTO dto)
     {
+        Optional<Account> existingAccount = accountRepository.findAccountByEmail(dto.email);
+        if(existingAccount.isPresent())
+        {
+            throw new ResponseStatusException(HttpStatus.CONFLICT,"Account with email already exists");
+        }
+        AccountPermission permissionLevel = AccountPermission.CUSTOMER;
+        //Guard for creating something of a high level
         if(dto.permission != null)
         {
-            var ordinalPermissionValue = AccountPermission.valueOf(dto.permission);
-            if(ordinalPermissionValue != AccountPermission.CUSTOMER)
+            var requestedPerm = AccountPermission.valueOf(dto.permission);
+            if(requestedPerm != AccountPermission.CUSTOMER)
             {
-
+                var creator = accountRepository.findById(dto.userID);
+                if(creator.isEmpty() || creator.get().getPermission() != AccountPermission.ADMIN)
+                {
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Account creation of high permission must be made by administrator");
+                }
+                permissionLevel = requestedPerm;
             }
         }
+        Account newAccount = new Account(YorkUtils.GenerateUUID(),permissionLevel,dto.email,dto.password);
+        productOrderRepository.save(newAccount.getCart());
+        accountRepository.save(newAccount);
+        return newAccount;
 
     }
 }
