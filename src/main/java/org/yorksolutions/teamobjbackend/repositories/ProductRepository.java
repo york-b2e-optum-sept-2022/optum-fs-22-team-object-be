7b57f4cf449d7336c216074a1c761efb99771acf
Max Feige <mfeige001@gmail.com>
2022-11-09 10:46:09 -0600

    Fixed coupon query code

--- src/main/java/org/yorksolutions/teamobjbackend/repositories/ProductRepository.java
+++ src/main/java/org/yorksolutions/teamobjbackend/repositories/ProductRepository.java
@@ -12,7 +12,7 @@ public interface ProductRepository extends CrudRepository<Product,String>
 {
     Iterable<Product> findAllByProductIDIn(Iterable<String> productIDs);
 
-    @Query(value = "SELECT * FROM product WHERE productid IN (SELECT product_productid FROM product_coupon_list WHERE code = 'Test Coupon Code')", nativeQuery = true)
+    @Query(value = "SELECT * FROM product WHERE productid IN (SELECT product_productid FROM product_coupon_list WHERE code = :code)", nativeQuery = true)
     Iterable<Product> findAllWithCouponCode(@Param("code") String code);
 
     Iterable<Product> findAllByDiscontinuedIsFalseAndCategories(String category);
